a7a9e8fdfa186c5dbd6d3f76e0620cbdb4315f30
jane5ford <elliana3256@gmail.com>
2019-11-07 21:51:34 +1000

    Cut
    Улучшение резки. Добавление резки только на доске

--- Assets/Scripts/CutGameObjects.cs
+++ Assets/Scripts/CutGameObjects.cs
@@ -2,34 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class CutGameObjects : MonoBehaviour
 {
     public List<GameObject> Pieces;
-    public int piece;
+    private int piece = 100;
     void Start()
     {
-        piece = Pieces.Count - 1;
+        piece = Pieces.Count-1;
     }
     void Update()
     {
-        
+
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        
+        
         if (collision.collider.tag == "Cut")
         {
             Pieces[piece].transform.position = new Vector3(Pieces[piece].transform.position.x - piece*0.02f, Pieces[piece].transform.position.y, Pieces[piece].transform.position.z);
             Pieces[piece].AddComponent<Rigidbody>();
-            piece -= 1;
-        }
-        if (piece < 0)
-        {
-            gameObject.GetComponent<BoxCollider>().enabled = false;
+            Pieces[piece].GetComponent<Rigidbody>().mass = 0.25f;
+            if (piece > -1) piece -= 1;
+
         }
     }
-    private void OnCollisionExit(Collision collision)
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.tag == "Board")
+        {
+            gameObject.GetComponent<CutGameObjects>().enabled = true;
+        }
     }
 }
