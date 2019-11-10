7d43c9670d6e33a98474231a893d3dba9cabb8e5
jane5ford <elliana3256@gmail.com>
2019-11-10 21:20:44 +1000

    Grater
    Процесс терки

---
+++ Assets/Scripts/GrateGameObject.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrateGameObject : MonoBehaviour
+{
+    public GameObject LittlePiece;
+    private GameObject littlePiece;
+    private bool Gratable;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Gratable && transform.localScale.z != 0)
+        {
+            littlePiece = GameObject.Instantiate(LittlePiece, gameObject.transform.position, Quaternion.identity) as GameObject;
+            gameObject.transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z - 0.01f);
+        }
+       
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Grate")
+        {
+            Gratable = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.tag == "Grate")
+        {
+            Gratable = false;
+        }
+    }
+}
