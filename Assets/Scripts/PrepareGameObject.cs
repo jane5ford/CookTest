fd95838692871a3ee089296a9c62fcf00a89d697
jane5ford <elliana3256@gmail.com>
2019-11-11 00:00:20 +1000

    Cooking process
    Скрипт для перехода с одного состояния на другое

---
+++ Assets/Scripts/PrepareGameObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrepareGameObject : MonoBehaviour
+{
+    public GameObject NewForm;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Cooking")
+        {
+            GameObject.Instantiate(NewForm, gameObject.transform.position, Quaternion.identity);
+            gameObject.SetActive(false);
+        }
+        
+    }
+}
