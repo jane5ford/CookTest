7d732b92a4d873f08ad44136b752c7a9078b513a
jane5ford <elliana3256@gmail.com>
2019-11-27 16:22:04 +1000

    Fixed Cut Game Object
    Улучшила работу резки продукта

--- Assets/Scripts/CutGameObjects.cs
+++ Assets/Scripts/CutGameObjects.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Valve.VR.InteractionSystem;
 
 public class CutGameObjects : MonoBehaviour
 {
     public List<GameObject> Pieces;
-    private int piece = 100;
+    private int piece;
+    private bool cuted = false;
     void Start()
     {
         piece = Pieces.Count-1;
@@ -16,18 +17,32 @@ public class CutGameObjects : MonoBehaviour
 
     }
 
+
     private void OnCollisionEnter(Collision collision)
     {
         
         
-        if (collision.collider.tag == "Cut")
+        if (collision.collider.tag == "Cut" && !(cuted))
         {
             Pieces[piece].transform.position = new Vector3(Pieces[piece].transform.position.x - piece*0.02f, Pieces[piece].transform.position.y, Pieces[piece].transform.position.z);
-            Pieces[piece].AddComponent<Rigidbody>();
+            Pieces[piece].AddComponent<Throwable>();
             Pieces[piece].GetComponent<Rigidbody>().mass = 0.25f;
-            if (piece > -1) piece -= 1;
+            gameObject.GetComponent<BoxCollider>().size = new Vector3(1,1,(float)1/Pieces.Count);
+            Debug.Log(1 / Pieces.Count);
+            cuted = true;
+            if (piece > 0) { piece -= 1; }
+            else
+            {
+                Destroy(gameObject.GetComponent<Throwable>());
+                Destroy(gameObject.GetComponent<Rigidbody>());
+            }
 
         }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        cuted = false;
     }
 
     private void OnTriggerEnter(Collider other)
