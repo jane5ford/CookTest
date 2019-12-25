e00da8cd7c7950a249d7b88f9b7b4df6661076e8
jane5ford <elliana3256@gmail.com>
2019-12-25 20:42:19 +1000

    Pouring and filling

--- Assets/Scripts/Filling.cs
+++ Assets/Scripts/Filling.cs
@@ -5,25 +5,43 @@ using UnityEngine;
 public class Filling : MonoBehaviour
 {
     public GameObject content;
+    private bool isPouring;
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (content.transform.localScale.x >= 0.1f && content.transform.localScale.x < 0.9f && isPouring)
+        {
+            float increase = 0.1f * Time.deltaTime;
+            content.transform.localScale += new Vector3(increase, increase, increase);
+        }
+        
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        if (collision.collider.tag == "WaterDrop")
+        if (other.tag == "Filling")
         {
-            if (content.transform.localScale != new Vector3(1f , 1f, 1f))
+            isPouring = true;
+            if (content.transform.localScale.x == 0)
             {
-                content.transform.localScale *= 1.1f;
+                content.SetActive(true); 
+                content.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
             }
+        }
+        
+    }
+    
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Filling")
+        {
+            //isPouring = false;
         }
     }
 }
