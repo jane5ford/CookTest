dd083d6b280a3df3168972f71cd19ece0a320e41
AlinaTeriakova <38549709+AlinaTeriakova@users.noreply.github.com>
2020-01-16 05:56:27 +1000

    Zzzzz

---
+++ Assets/Scripts/TimeBar.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class TimeBar : MonoBehaviour
+{
+
+    public Image bar;
+    public float fill;
+
+    void Start()
+    {
+        fill = 1f;
+    }
+
+
+    void Update()
+    {
+        fill -= Time.deltaTime * 0.1f;
+        bar.fillAmount = fill;
+    }
+}
